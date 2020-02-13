146328ed8020508ab64fd3598177d28f252eb2ba
Stiven <starias@ucm.es>
2020-02-13 17:46:28 +0100

    Nuevo repositorio para la practica1. Actualizado Sierpinski y añadido TrianguloRGB(ejercicio 3)

---
+++ IG1App/Scene.cpp
@@ -0,0 +1,70 @@
+#include "Scene.h"
+#include "CheckML.h"
+#include <gtc/matrix_transform.hpp>  
+#include <gtc/type_ptr.hpp>
+
+using namespace glm;
+//-------------------------------------------------------------------------
+
+void Scene::init()
+{ 
+	setGL();  // OpenGL settings
+
+	// allocate memory and load resources
+    // Lights
+    // Textures
+
+    // Graphics objects (entities) of the scene
+	gObjects.push_back(new EjesRGB(400.0));
+
+#pragma region Ejercicio1
+	gObjects.push_back(new Poligono(3, 500)); //Triángulo
+	gObjects.back()->setMColor({ 255.0, 255.0, 0.0, 1.0 }); //Asignamos amarillo al triángulo
+	gObjects.push_back(new Poligono(360, 500)); //Circunferencia
+	gObjects.back()->setMColor({255.0, 0.0, 255.0, 1.0}); //Asignamos magenta al triángulo
+#pragma endregion
+#pragma region Ejercicio2
+	gObjects.push_back(new Sierpinski(500, 10000));
+	gObjects.back()->setMColor(dvec4(1.0, 1.0, 0.0, 1.0)); //Asignamos magenta a Sierpinski
+#pragma endregion
+#pragma region Ejercicio3
+	gObjects.push_back(new TrianguloRGB(100));
+#pragma endregion
+}
+//-------------------------------------------------------------------------
+void Scene::free() 
+{ // release memory and resources   
+
+	for (Abs_Entity* el : gObjects)
+	{
+		delete el;  el = nullptr;
+	}
+}
+//-------------------------------------------------------------------------
+void Scene::setGL() 
+{
+	// OpenGL basic setting
+	glClearColor(1.0, 1.0, 1.0, 1.0);  // background color (alpha=1 -> opaque)
+	glEnable(GL_DEPTH_TEST);  // enable Depth test 
+
+}
+//-------------------------------------------------------------------------
+void Scene::resetGL() 
+{
+	glClearColor(.0, .0, .0, .0);  // background color (alpha=1 -> opaque)
+	glDisable(GL_DEPTH_TEST);  // disable Depth test 	
+}
+//-------------------------------------------------------------------------
+
+void Scene::render(Camera const& cam) const 
+{
+	cam.upload();
+
+	for (Abs_Entity* el : gObjects)
+	{
+	  el->render(cam.viewMat());
+	}
+}
+//-------------------------------------------------------------------------
+
+
